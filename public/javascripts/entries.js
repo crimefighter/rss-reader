726e687fa20ba31d1dc21ce1c5f379dac01b8d8a
Mike <tenore.hatemail@gmail.com>
2010-05-10 19:44:56 +0300

    dialog added

--- public/javascripts/entries.js
+++ public/javascripts/entries.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
+  $("#add_subscription").dialog({
+  });
   $(".add_feed_toggle").click(function() {
-    $(this).next("#new_feed").toggle();
+    $("#add_subscription").dialog('open');
     return false;
   });
 
-  $(".feed_menu_icon").click(function() {
-    $(this).next(".feed_menu").toggle();
-    return false;
+  $("div.feed_properties").each(function() {
+    feed_name = $(this).parent().find("a.feed").html();
+    $open_link = $(this).prev("a.feed_menu_icon");
+    var that = $(this);
+    that.dialog({
+      title: feed_name + " properties",
+      autoOpen: false,
+      resizeable: false,
+      modal: true,
+      hight: 300,
+      width: 400
+    });
+    $open_link.click(function() {
+      that.dialog('open');
+    });
   });
+
   $("ul#feeds li").bind("mouseenter", function() {
     $(this).find(".feed_menu_icon").show();
   }).bind("mouseleave", function() {
